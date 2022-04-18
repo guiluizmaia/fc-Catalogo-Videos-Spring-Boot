ce3793802cd5a278f87e979d6c9c8c3903bd6418
guiluizmaia <guilhermemaia201450@gmail.com>
2022-04-18 09:47:25 -0300

    fix: repository and entity

---
+++ src/main/java/com/fullcycle/CatalogoVideos/infraestructure/data/SpringDataCategoryRepository.java
@@ -0,0 +1,8 @@
+package com.fullcycle.CatalogoVideos.infraestructure.data;
+
+import java.util.UUID;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpringDataCategoryRepository extends CrudRepository<null, UUID>{
+    
+}
---
+++ src/main/java/com/fullcycle/CatalogoVideos/infraestructure/persistence/CategoryPersistence.java
@@ -0,0 +1,39 @@
+package com.fullcycle.CatalogoVideos.infraestructure.persistence;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryPersistence {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "VARBINARY(16)")
+    private UUID id;
+    
+    @Column
+    @NotNull(message = "name can not be null")
+    @NotBlank(message = "name can not be blank")
+    private String name;
+    
+    @Column
+    private String description;
+
+    @Column
+    private Boolean isActive = true;
+}
