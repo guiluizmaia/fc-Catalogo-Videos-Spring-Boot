909ed4d7405da9ebdff9844710bb9a2267f3d61a
guiluizmaia <guilhermemaia201450@gmail.com>
2022-07-15 12:30:44 -0300

    feat: update category

--- application/src/test/java/com/fullcycle/admin/catalogo/application/category/update/UpdateCategoryUseCaseTest.java
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/category/update/UpdateCategoryUseCaseTest.java
@@ -61,7 +61,7 @@ public class UpdateCategoryUseCaseTest {
                             && Objects.equals(expectedIsActive, aUpdatedCategory.isActive())
                             && Objects.equals(expectedId, aUpdatedCategory.getId())
                             && Objects.equals(aCategory.getCreatedAt(), aUpdatedCategory.getCreatedAt())
-                            && aCategory.getUpdatedAt().isBefore(aUpdatedCategory.getUpdatedAt())
+                            && Objects.equals(aCategory.getUpdatedAt(), aUpdatedCategory.getUpdatedAt())
                             && Objects.isNull(aUpdatedCategory.getDeletedAt())
         ));
 
