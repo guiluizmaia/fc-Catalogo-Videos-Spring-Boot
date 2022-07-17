c46d5b7d3a2dad8639733b258813359dc80881a7
guiluizmaia <guilhermemaia201450@gmail.com>
2022-07-17 15:31:58 -0300

    feat: test complete

--- application/src/test/java/com/fullcycle/admin/catalogo/application/category/update/UpdateCategoryUseCaseTest.java
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/category/update/UpdateCategoryUseCaseTest.java
@@ -43,7 +43,7 @@ public class UpdateCategoryUseCaseTest {
         );
 
         when(categoryGateway.findById(eq(expectedId)))
-                .thenReturn(Optional.of(aCategory));
+                .thenReturn(Optional.of(aCategory.clone()));
 
         when(categoryGateway.update(any()))
                 .thenAnswer(returnsFirstArg());
@@ -61,7 +61,7 @@ public class UpdateCategoryUseCaseTest {
                             && Objects.equals(expectedIsActive, aUpdatedCategory.isActive())
                             && Objects.equals(expectedId, aUpdatedCategory.getId())
                             && Objects.equals(aCategory.getCreatedAt(), aUpdatedCategory.getCreatedAt())
-                            && Objects.equals(aCategory.getUpdatedAt(), aUpdatedCategory.getUpdatedAt())
+                            && aCategory.getUpdatedAt().isBefore(aUpdatedCategory.getUpdatedAt())
                             && Objects.isNull(aUpdatedCategory.getDeletedAt())
         ));
 
